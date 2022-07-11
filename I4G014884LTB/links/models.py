5042381aee601650f7d980906cb27cf45b524155
oriafo <abrahamibharunujele@gmail.com>
2022-07-10 23:03:26 -0700

    modified the custom save method

--- I4G014884LTB/links/models.py
+++ I4G014884LTB/links/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.template.defaultfilters import slugify
+from links.utils import generate_random_id
 
-
-# Create your models here.
 class Link(models.Model):
     target_url = models.URLField(max_length=200)
     description = models.CharField(max_length=200)
@@ -14,9 +13,11 @@ class Link(models.Model):
 
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.identifier)
-        super(Link, self).save(*args, **kwargs)
-        pass 
+        if not id:
+            self.slug = slugify(self.identifier)
+            super(Link, self).save(*args, **kwargs)
+        else:
+            pass 
 
     def __str__(self):
-        return f"Short Url for: {self.target_url} is {self.identifier}"
+        return self.identifier
